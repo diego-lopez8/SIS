f954a3a265a4765063548de1d0d872052c8929e4
diego-lopez8 <diego50421@gmail.com>
2021-08-16 09:40:52 -0400

    added project structure

---
+++ bin/CMakeFiles/SIS.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/diego/Projects/SIS/src/AcademicSessionReport.cpp" "CMakeFiles/SIS.dir/AcademicSessionReport.cpp.o" "gcc" "CMakeFiles/SIS.dir/AcademicSessionReport.cpp.o.d"
+  "/home/diego/Projects/SIS/src/Class.cpp" "CMakeFiles/SIS.dir/Class.cpp.o" "gcc" "CMakeFiles/SIS.dir/Class.cpp.o.d"
+  "/home/diego/Projects/SIS/src/Instructor.cpp" "CMakeFiles/SIS.dir/Instructor.cpp.o" "gcc" "CMakeFiles/SIS.dir/Instructor.cpp.o.d"
+  "/home/diego/Projects/SIS/src/Person.cpp" "CMakeFiles/SIS.dir/Person.cpp.o" "gcc" "CMakeFiles/SIS.dir/Person.cpp.o.d"
+  "/home/diego/Projects/SIS/src/Student.cpp" "CMakeFiles/SIS.dir/Student.cpp.o" "gcc" "CMakeFiles/SIS.dir/Student.cpp.o.d"
+  "/home/diego/Projects/SIS/src/main.cpp" "CMakeFiles/SIS.dir/main.cpp.o" "gcc" "CMakeFiles/SIS.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bin/CMakeFiles/SIS.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SIS.dir/AcademicSessionReport.cpp.o"
+  "CMakeFiles/SIS.dir/AcademicSessionReport.cpp.o.d"
+  "CMakeFiles/SIS.dir/Class.cpp.o"
+  "CMakeFiles/SIS.dir/Class.cpp.o.d"
+  "CMakeFiles/SIS.dir/Instructor.cpp.o"
+  "CMakeFiles/SIS.dir/Instructor.cpp.o.d"
+  "CMakeFiles/SIS.dir/Person.cpp.o"
+  "CMakeFiles/SIS.dir/Person.cpp.o.d"
+  "CMakeFiles/SIS.dir/Student.cpp.o"
+  "CMakeFiles/SIS.dir/Student.cpp.o.d"
+  "CMakeFiles/SIS.dir/main.cpp.o"
+  "CMakeFiles/SIS.dir/main.cpp.o.d"
+  "SIS"
+  "SIS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SIS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
