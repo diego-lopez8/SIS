9938ff6e6fd18ddd7ca15b81e53267712d55f798
diego-lopez8 <diego50421@gmail.com>
2021-08-18 19:17:20 -0400

    created addClass() function

--- include/Instructor.h
+++ include/Instructor.h
@@ -1,10 +1,31 @@
+/*
+Instructor.h
+Author: Diego Lopez
+last updated: 8/18/2021
+This file contains headers for the Instructor class. 
+*/
+
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
 #include "Person.h"
+#include <vector>
+#include <iostream>
 
+class Class;
 
 class Instructor : public Person {
-    Instructor();
+public:
+    friend std::ostream & operator<<(std::ostream & os, const Instructor & aInstructor);
+
+    Instructor(const std::string & nameIn, int universityIDIn, const std::string & netIDIn);
+
+    bool addClass(Class & aClass);
+
+private:
+    std::vector<const Class*> schedule;
 };
+
+std::ostream & operator<<(std::ostream & os, const Instructor & aInstructor);
+
 
 #endif
--- src/Instructor.cpp
+++ src/Instructor.cpp
@@ -1,7 +1,40 @@
+/*
+Instructor.cpp
+Author: Diego Lopez
+last updated: 08/18/2021
+This file contains the implementations for methods of the Instructor class. 
+
+LEFT TO IMPLEMENT: 
+- addClass() should check for time conflicts 
+- Instructors should be the only class allowed to edit a student's Academic Report (grade)
+*/
 #include "Person.h"
 #include <string>
 #include "Instructor.h"
+#include <iostream>
+#include "Class.h"
 
 using namespace std;
 
-Instructor::Instructor(){}
+Instructor::Instructor(const string & nameIn, int universityIDIn, const string & netIDIn) 
+    : Person(nameIn, universityIDIn, netIDIn){}
+
+bool Instructor::addClass(Class & aClass) {
+    for (const Class* const elem : schedule) {
+        if (elem == &aClass) {
+            cout << "ERROR: This class is already being taught by " << this->getName() << endl;
+            return false;
+        }
+    }
+    cout << "Success! Added " << aClass.getCourseTitle() << " to " << this->getName() << "s schedule" << endl;
+    schedule.push_back(&aClass);
+    return true;
+}
+
+ostream & operator<<(ostream & os, const Instructor & aInstructor) {
+    os << aInstructor.getName() << ": " << aInstructor.getnetID() << endl;
+    for (const Class* const elem : aInstructor.schedule) {
+        os << elem; 
+    }
+    return os;
+}
